b75a97ee430813b50799ee9d6dbde6cdfb6271dd
Anthony Ingle <ingle.anthony@icloud.com>
2022-01-17 00:22:36 -0500

    changed meta colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,8 +38,8 @@ const Home: NextPage = () => {
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#162645" />
+        <meta name="msapplication-TileColor" content="#162645" />
         <meta name="theme-color" content="#ffffff" />
         <meta property="og:title" content="Anthony Ingle" />
         <meta property="og:type" content="profile" />
@@ -57,7 +57,7 @@ const Home: NextPage = () => {
       </Head>
 
       <header className={styles.header}>
-        <Wave color1="#162645" color2="#7184A9" />
+        <Wave color1="#162645" color2="#162645" />
         <div className={styles.imageContainer}>
           <Image
             src="/portrait.jpg"
