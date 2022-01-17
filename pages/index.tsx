88536e2c2d8562846cf8998fc58d82306c78aba7
Anthony Ingle <ingle.anthony@icloud.com>
2022-01-17 00:17:09 -0500

    updated meta tags and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import styles from "../styles/Home.module.css";
 import Wave from "../components/Wave";
 import ImageLink from "../components/ImageLink";
 import RowLink from "../components/RowLink";
-import Portrait from "../images/portrait.jpg";
 
 import GitHubSvg from "../svg/github.svg";
 import LinkedInSvg from "../svg/linkedin.svg";
@@ -21,13 +20,50 @@ const Home: NextPage = () => {
       <Head>
         <title>Anthony Ingle</title>
         <meta name="description" content="Anthony Ingle's Profile" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta property="og:title" content="Anthony Ingle" />
+        <meta property="og:type" content="profile" />
+        <meta property="og:profile:first_name" content="Anthony" />
+        <meta property="og:profile:last_name" content="Ingle" />
+        <meta property="og:profile:gender" content="male" />
+        <meta property="og:url" content="https://anthonyingle.me" />
+        <meta
+          property="og:description"
+          content="Full-stack Software Engineer"
+        />
+        <meta property="og:image" content="/portrait.jpg" />
+        <meta property="og:image:width" content="1024" />
+        <meta property="og:image:height" content="1024" />
       </Head>
 
       <header className={styles.header}>
         <Wave color1="#162645" color2="#7184A9" />
         <div className={styles.imageContainer}>
-          <Image src={Portrait} alt="Anthony Ingle" className={styles.image} />
+          <Image
+            src="public/portrait.jpg"
+            alt="Anthony Ingle"
+            className={styles.image}
+          />
         </div>
       </header>
       <main className={styles.main}>
